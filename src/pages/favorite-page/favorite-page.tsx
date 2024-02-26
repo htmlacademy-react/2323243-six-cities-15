435a6cf44a1fb4d9317abcd343d42e05891fbaa3
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-02-26 14:02:13 +0100

    Исправлены требования линтера Реализован глобальный стейт с Redux Улучшена логика компонента Filter WIP: Загрузка данных с сервера Исправлено отображение города в Заголовке Исправлен счётчик взрослых и детей Добавлена подсветка активного оффера на карте Реализована сортировка офферов Реализована фильтрация по городам Улучшена инициализация данных Оптимизирован рендер в зависимости от города Типизация и моки для офферов Добавлены координаты городов в константы Изменена структура данных на странице OfferPage Изменена логика алгоритма сортировки Реализован UI для списка сортировки

---
+++ src/pages/favorite-page/favorite-page.tsx
@@ -0,0 +1,68 @@
+import { Helmet } from 'react-helmet-async';
+
+import CardList from '../../components/card-list/card-list';
+
+import {Offer} from '../../types/offer';
+import FavoritesByCity from '../../types/favorites-by-city';
+
+
+type FavoritePageProps = {
+  offers: Array<Offer>;
+}
+
+function getFavoriteCities(favOffers: Array<Offer>): FavoritesByCity {
+  const result = favOffers.reduce<FavoritesByCity>((acc, value) => {
+    const city = value.city.name;
+    if (!(city in acc)) {
+      acc[city] = [];
+    }
+    acc[city].push(value);
+    return acc;
+  }, {});
+  return result;
+}
+
+function FavoritePage({ offers }: FavoritePageProps): JSX.Element {
+  const favoriteOffers = offers.filter((offer) => offer.isFavorite);
+  const favoritesByCity = getFavoriteCities(favoriteOffers);
+
+  return (
+    <div className="page">
+      <Helmet>
+        <title>{'6 cities - favorites'}</title>
+      </Helmet>
+      <main className="page__main page__main--favorites">
+        <div className="page__favorites-container container">
+          <section className="favorites">
+            <h1 className="favorites__title">Saved listing</h1>
+            <ul className="favorites__list">
+              {Object.entries(favoritesByCity).map(([city, favoriteList]) => (
+
+                < li key={city} className="favorites__locations-items">
+                  <div className="favorites__locations locations locations--current">
+                    <div className="locations__item">
+                      <a className="locations__item-link" href="#">
+                        <span>{city}</span>
+                      </a>
+                    </div>
+                  </div>
+                  <div className="favorites__places">
+                    <CardList elementType='favorite' offers={favoriteList} />
+                  </div>
+                </li>
+              ))}
+            </ul>
+
+          </section>
+        </div>
+      </main >
+      <footer className="footer container">
+        <a className="footer__logo-link" href="main.html">
+          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
+        </a>
+      </footer>
+    </div >
+  );
+}
+
+export default FavoritePage;
